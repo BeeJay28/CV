64ad22cbbacb7aacf2c05abf7b55c1bf8718b844
Artur Herdt <herdt.artur28@yahoo.com>
2022-10-09 18:44:45 +0200

    minor changes

--- script.js
+++ script.js
@@ -7,30 +7,30 @@ const sectionEls = document.querySelectorAll("section");
 
 const obsCallback = function (entries) {
   const [entry] = entries;
+  if (!entry.isIntersecting) return;
 
-  const obsSectionNo = entry.target.querySelector(".section-title")?.id.slice(-1);
+  const obsSectionNo = entry.target.id.slice(-1);
   if (!obsSectionNo) return;
+  console.log(obsSectionNo);
 
   const obsNav = document.querySelector(`.nav-item[data-section="${obsSectionNo}"]`);
 
-  if (entry.isIntersecting) {
-    navItemEls.forEach(el => el.classList.remove("nav-item--selected"));
-    obsNav.classList.add("nav-item--selected");
-  }
+  navItemEls.forEach(el => el.classList.remove("nav-item--selected"));
+  obsNav.classList.add("nav-item--selected");
 };
 
 const obsOptions = {
   root: null,
-  threshold: [0.9, 1],
+  threshold: [0.5, 1],
 };
 
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 sectionEls.forEach(section => observer.observe(section));
 
-navLinks.addEventListener("mouseover", function (e) {
-  navItemEls.forEach(navItem => (navItem.style.opacity = 0.5));
-  e.target.closest(".nav-item").style.opacity = 1;
-});
+// navLinks.addEventListener("mouseover", function (e) {
+//   navItemEls.forEach(navItem => (navItem.style.opacity = 0.5));
+//   e.target.closest(".nav-item").style.opacity = 1;
+// });
 
 // navLinks.addEventListener("mouseleave");
 
